9cdd7e5254c873f54f839359b83d8dc6cb02145d
Diego <diegofabrega18@gmail.com>
2024-04-20 20:26:32 -0400

    UPDATE: Add filename not provided and filenotfound errors description

--- utils/errorsDictionary.js
+++ utils/errorsDictionary.js
@@ -1,6 +1,8 @@
 const ERROR_DICTIONARY = {
   EXT_NOT_ALLOWED:
     "The extension of the file is not allowed, you must send an admitted file.",
+  FILENAME_NOT_PROVIDED: "You didn't provide the filename to work on",
+  FILE_NOT_FOUND: "The file doesn't exist in the current commands directory",
 };
 
 export default ERROR_DICTIONARY;
