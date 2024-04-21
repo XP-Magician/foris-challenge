b73370f1ffca12a61b2ce345f1a683fa7c62f110
Diego <diegofabrega18@gmail.com>
2024-04-20 20:00:55 -0400

    ADD : Utils directory, config constants

---
+++ utils/config.js
@@ -0,0 +1,11 @@
+import path from "node:path";
+import os from "node:os";
+
+const config = {
+  COMMANDS_DIRECTORY: path.join("..", "commands"),
+  ALLOWED_EXTENSIONS: ["txt"],
+  NEXT_LINE: os.EOL,
+  CHARSET: "utf8",
+};
+
+export default config;
