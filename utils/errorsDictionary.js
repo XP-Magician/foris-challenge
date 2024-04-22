13e877fd3978d678f8ed7964b0629946cbe1dc77
Diego <diegofabrega18@gmail.com>
2024-04-21 23:53:16 -0400

    UPDATE: Add errors messages according to the requirements and validations

--- utils/errorsDictionary.js
+++ utils/errorsDictionary.js
@@ -1,8 +1,16 @@
 const ERROR_DICTIONARY = {
   EXT_NOT_ALLOWED:
     "The extension of the file is not allowed, you must send an admitted file.",
-  FILENAME_NOT_PROVIDED: "You didn't provide the filename to work on",
-  FILE_NOT_FOUND: "The file doesn't exist in the current commands directory",
+  FILENAME_NOT_PROVIDED: "You didn't provide the filename to work on.",
+  FILE_NOT_FOUND: "The file doesn't exist in the current commands directory.",
+  STUDENT_NOT_REGISTERED:
+    "The presence must have a registered student with 'Student' command before. ",
+  DIFF_NOT_ENOUGH:
+    "The presence isn't valid because it's fewer than five minutes.",
+  ENTER_GREATHER_THAN_LEFT:
+    "The presence isn't valid because the enter time is greather than the left time.",
+  INVALID_COMMAND_FORMAT:
+    "The command don't have a valid format, check your entries carefully.",
 };
 
 export default ERROR_DICTIONARY;
