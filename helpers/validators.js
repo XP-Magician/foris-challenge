4905716b39578c57bae76ee64d4b59a7ca384834
Diego <diegofabrega18@gmail.com>
2024-04-21 23:08:52 -0400

    FIX: The student enter hour must be between 08:00 and 09:59 in the earlier case

--- helpers/validators.js
+++ helpers/validators.js
@@ -1,5 +1,5 @@
 export const VALIDATORS = {
   STUDENT_COMMAND: /^Student [\w]+$/,
   PRESENCE_COMMAND:
-    /^Presence [\w]+ [1-7] ((0[6-9])?(1[0-9])?(2[0-3])?:([0-5][0-9]\s)){2}[A-Z]\d{3}$/,
+    /^Presence [\w]+ [1-7] ((0[8-9])?(1[0-9])?(2[0-3])?:([0-5][0-9]\s)){2}[A-Z]\d{3}$/,
 };
