cecc6dbea2fbe27eacca7068a7016841eb73b65e
Diego <diegofabrega18@gmail.com>
2024-04-22 22:46:37 -0400

    UPDATE: Change directory default location to match with main app instance

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ import path from "node:path";
 import os from "node:os";
 
 const config = {
-  COMMANDS_DIRECTORY: path.join("..", "commands"),
+  COMMANDS_DIRECTORY: path.join("commands"),
   ALLOWED_EXTENSIONS: [".txt"],
   NEXT_LINE: os.EOL,
   CHARSET: "utf8",
