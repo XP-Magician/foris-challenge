474ce36e9e75676dd27df91d5e6883eadc418bf7
Diego <diegofabrega18@gmail.com>
2024-04-22 22:47:34 -0400

    UPDATE: Add app initialization, using the compiler created in tools

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Futura aplicación y procesamiento");
+import Compiler from "./tools/commandsCompiler.js";
+
+// Must recibe a node param with the name of the file , for example : node app.js "myfile.txt" , you can change the default location to
+// search the file in utils/config : COMMANDS_DIRECTORY constant.
+const initialize = async () => await Compiler();
+initialize();
