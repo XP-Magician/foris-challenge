8e4516547540477d7c3ec66febf8c93a46d75c57
Diego <diegofabrega18@gmail.com>
2024-05-12 18:36:58 -0400

    UPDATE: Added automatic single minutes calculator method on constructor, valid presence checker

--- entities/Presence.js
+++ entities/Presence.js
@@ -1,14 +1,25 @@
-import { VALIDATOR_PRESENCE_DETAILS } from "../helpers/validators";
+import { DateTime } from "luxon";
+import ERROR_DICTIONARY from "../utils/errorsDictionary.js";
+import { VALIDATORS } from "../helpers/validators.js";
 // New entity class added for making the whole process more flexible
 export class Presence {
-  // constructor for defining the Presence
-
-  constructor(room, minutes, student_id, enter_hour, left_hour) {
+  constructor(room, student_id, enter_hour, left_hour, day) {
     this.room = room;
-    this.minutes = minutes;
     this.student_id = student_id;
     this.enter_hour = enter_hour;
     this.left_hour = left_hour;
+    this.day = day;
+    this.minutes = this.calculateMinutes(enter_hour, left_hour);
+  }
+
+  // Used for calculating the presence minutes according to the enter and left hour
+  calculateMinutes(enter_hour_raw, left_hour_raw) {
+    const enter_hour = DateTime.fromISO(enter_hour_raw);
+    const left_hour = DateTime.fromISO(left_hour_raw);
+    const minutes_presence =
+      (left_hour.toMillis() - enter_hour.toMillis()) /
+      VALIDATORS.MILIS_TO_MINUTES;
+    return minutes_presence;
   }
 
   getPresence() {
@@ -18,15 +29,34 @@ export class Presence {
       student_id: this.student_id,
       enter_hour: this.enter_hour,
       left_hour: this.left_hour,
+      day: this.day,
     };
   }
 
-  /* See if the presence is valid according to :
-   *Student was previously registered
-   *Presence is at least of 5 minutes
-   *Enter_hour is fewer than left_hour
-   */
   isValidPresence(list_students) {
-    return VALIDATOR_PRESENCE_DETAILS(list_students, this.getPresence());
+    const presence_to_verify = this.getPresence();
+    // Student was previously registered?
+    let student_was_registered = false;
+    list_students.forEach((student_object) => {
+      if (student_object.student_id === presence_to_verify.student_id) {
+        student_was_registered = true;
+        return;
+      }
+    });
+    let { enter_hour, left_hour } = presence_to_verify;
+    enter_hour = DateTime.fromISO(enter_hour);
+    left_hour = DateTime.fromISO(left_hour);
+    //Enter_hour is fewer than left_hour ?
+    if (left_hour < enter_hour)
+      return ERROR_DICTIONARY.ENTER_GREATHER_THAN_LEFT;
+
+    // Presence is at least of 5 minutes ?
+    if (left_hour.toMillis() - enter_hour.toMillis() < VALIDATORS.MIN_DIFF_TIME)
+      return ERROR_DICTIONARY.DIFF_NOT_ENOUGH;
+
+    // Last validation to check whether the student was registered or not
+    return student_was_registered
+      ? true
+      : ERROR_DICTIONARY.STUDENT_NOT_REGISTERED;
   }
 }
