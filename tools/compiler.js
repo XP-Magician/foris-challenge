109f83927b658035ca8b32748924550700eb0dc2
Diego <diegofabrega18@gmail.com>
2024-05-13 16:09:40 -0400

    UPDATE: Added final  result processment  according to de travel grouping clause

--- tools/compiler.js
+++ tools/compiler.js
@@ -1,5 +1,6 @@
-import { groupByStudent, groupByRoom } from "./groupers.js";
+import { groupByStudent, groupByRoom, groupByTravel } from "./groupers.js";
 import config from "../utils/config.js";
+import ERROR_DICTIONARY from "../utils/errorsDictionary.js";
 // Make a reusable method for calculating final minutes and days presences, no matter the group clause
 const genericStudentResult = (student_presences, student_id) => {
   let total_minutes = 0;
@@ -63,4 +64,47 @@ export const getRoomGroupResult = async () => {
   });
 
   return processed_room_presences;
+};
+
+export const getTravelsGroupResult = async () => {
+  let { travels_student, discarded } = await groupByTravel();
+  const travels_student_proccesed = []; // It's going to contain something like : Marco : F101->R902->...
+
+  // First we sort the travels in order : day -> enter_hour
+  let travels_student_keys = Object.keys(travels_student);
+  travels_student_keys.forEach((student) => {
+    let travel_string = `${student} : `;
+    let travels_student_sorted = travels_student[student].Travels;
+    if (travels_student_sorted === undefined) {
+      // The student don't have any rooms visited yet
+      travel_string += "-";
+    } else {
+      travels_student_sorted = travels_student_sorted
+        // First sort by day
+        .sort((t1, t2) => t1.day - t2.day)
+        // Then sort by enter_hour
+        .sort((t1, t2) => {
+          if (t1.day === t2.day) {
+            return t1.enter_hour - t2.enter_hour;
+          }
+          return 0;
+        });
+      // Once the days and hours of Travels are propperly sorted, we can make the route of the student
+
+      let current_day = travels_student_sorted[0].day; // To check wheter the travel was in the same day for put -> or into a different one to put ,
+      travels_student_sorted.forEach((travel, index) => {
+        travel_string += travel.room;
+        const next_travel = travels_student_sorted[index + 1]; // For avoid putting a , or a -> at the final of the string if it's the last register
+        if (next_travel) {
+          travel_string +=
+            next_travel.day === current_day
+              ? ERROR_DICTIONARY.ARROW_STRING
+              : ERROR_DICTIONARY.COMMA_STRING;
+          current_day = next_travel.day;
+        }
+      });
+    }
+    travels_student_proccesed.push(travel_string);
+  });
+  return travels_student_proccesed;
 };
